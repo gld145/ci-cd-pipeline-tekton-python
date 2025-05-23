c775894823b3dbfe51910f7dc77c043969acd567
GLD145 <gld145@example.com>
2025-05-23 19:30:45 -0400

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from service import config  # ✅ Nécessaire pour que la ligne suivante fonctionne
 from service.extensions import talisman
 
+
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(config)
 
 talisman.init_app(app, force_https=False)
